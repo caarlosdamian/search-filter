4e3300115aa5a274a84910aeb39f8069f3207621
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-02-21 23:29:39 -0600

    Table not efficient way

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import { Users } from "./users";
 import "./App.css";
+import { Table } from "./components/Table";
 
 function App() {
   const [query, setQuery] = useState("");
+  const search = (data) => {
+    return data.filter(
+      (item) =>
+        item.first_name.toLowerCase().includes(query) ||
+        item.last_name.toLowerCase().includes(query) ||
+        item.email.toLowerCase().includes(query)
+    );
+  };
   return (
     <div className="App">
       <input
@@ -12,15 +21,7 @@ function App() {
         placeholder="Search..."
         onChange={(e) => setQuery(e.target.value)}
       />
-      <ul className="list">
-        {Users.filter((user) =>
-          user.first_name.toLowerCase().includes(query)
-        ).map((user) => (
-          <li key={user.id} className="listItem">
-            {user.first_name}
-          </li>
-        ))}
-      </ul>
+      <Table data={search(Users)} />
     </div>
   );
 }
